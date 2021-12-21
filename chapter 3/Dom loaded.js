8028847d4d66b97b9f548ec0c65ec46cd2ec4182
sundharwinston <sundharwinston@gmail.com>
2021-12-21 21:37:19 +0530

    dom

---
+++ chapter 3/Dom loaded.js
@@ -0,0 +1,14 @@
+<html>
+    <head>
+        <title> DOM Load</title>
+        <script type="text/javascript">
+            function load() {
+                console.log("DOM Loaded.");
+            }
+            document.addEventListener("DOMContentLoaded", load);
+        </script>
+    </head>
+    <body>
+      <h1>Domain Object Model</h1>
+    </body>
+</html>
