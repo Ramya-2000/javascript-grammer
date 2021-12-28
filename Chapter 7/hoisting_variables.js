68ebe7f1109502998289deb7eaa85046a610bf9d
sundharwinston <sundharwinston@gmail.com>
2021-12-28 23:31:50 +0530

    hoisting variable

---
+++ Chapter 7/hoisting_variables.js
@@ -0,0 +1,10 @@
+//var 
+x = 5;                         // Assign 5 to x                    
+console.log(x);                //5
+var x;                         // Declare x
+
+//let
+y = 5;                         // Assign 5 to y                   
+console.log(y);                // ReferenceError: Cannot access 'y' before initialization
+let y;                         // Declare y
+
