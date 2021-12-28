d4bd92aa8148250b16bc5e05cb8f4e5e620069c7
sundharwinston <sundharwinston@gmail.com>
2021-12-29 00:02:20 +0530

    comparison

---
+++ Chapter 8/comparison.js
@@ -0,0 +1,12 @@
+1 == 1 ;//true
+'1' == 1 ; //true Equality
+'1' == '1' ; //true
+1 === 1 ; //true
+'1' === 1 ; //flase Equality of value and type
+'1' === '1' ; //true
+1 != 1 ; //false
+1 != 2 ; //true Equality
+2 > 1 ; //true
+5 < 7 ; //true
+2 >= 1; //true
+2 <= 1; //flase
