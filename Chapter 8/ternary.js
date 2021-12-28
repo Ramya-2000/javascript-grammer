306830f75f394eb536f342106c7e72f65e2b59b9
sundharwinston <sundharwinston@gmail.com>
2021-12-29 00:10:23 +0530

    ternary

---
+++ Chapter 8/ternary.js
@@ -0,0 +1,9 @@
+let marks = 45;
+
+// check the condition
+let result = (marks >= 40) ? 'pass' : 'fail';
+
+var statement = 45;
+let rest = 40;
+rest = (rest >= statement) ? "pass" : "fail";
+console.log(result);
