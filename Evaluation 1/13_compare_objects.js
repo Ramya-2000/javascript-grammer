afa1f1497f67d1e1922b87fef8513248b0f7fd19
sundharwinston <sundharwinston@gmail.com>
2022-01-04 23:25:06 +0530

    compare objects

--- Evaluation 1/13_compare_objects.js
+++ Evaluation 1/13_compare_objects.js
@@ -1,11 +1,9 @@
 
-var arr=[
-let k1=({id: 1, name: “edison”}, {id: 1, name: “edison”})
- const k2=({id: 1, name: “edison”},{id: 2, name: “edison”})
-];
-Object.entries(k1).toString() === Object.entries(k2).toString();
-// true
+const arr1=[{id: 1, name: 'edison'}, {id: 1, name: 'edison'}];
+ const arr2=[{id: 1, name: 'edison'},{id: 2, name: 'edison'}];
+
+if(Object.entries(arr1).toString() === Object.entries(arr2).toString()) {
 console.log("true")
-else{
-    console.log("false");
+} else{
+    console.log("false")
 }
