ba68e25c2602fd57183d28e568354e1b725f6d8a
sundharwinston <sundharwinston@gmail.com>
2022-01-04 15:11:08 +0530

    Evaluation 1

--- Evaluation 1/12_duplicate.js
+++ Evaluation 1/12_duplicate.js
@@ -1,19 +1,11 @@
 
-var array = [
+var arr = [
     {id: 1, name: 'Stephen covey'},
     {id: 2, name: 'Robin Sharma' }, 
     {id: 3, name: 'Tolstoy'}, 
     {id: 3, name: 'Tolstoy'}, 
     {id: 5, name: 'James clear'}
 ];
-const uniqueIds = [];
-
-const unique = arr.filter(element => {
-  const isDuplicate = uniqueIds.includes(element.id);
-
-  if (!isDuplicate) {
-    uniqueIds.push(element.id);
-
-    return true;
-  }
-});
+const ids=arr.map(o => o.id)
+const filtered=arr.filter(({id},index) => !ids.includes(id,index+1))
+console.log(filtered)
